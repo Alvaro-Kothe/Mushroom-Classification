45eba183db136b837290a27feebb0ab6c2f136ec
Alvaro-Kothe <kothe65@gmail.com>
2023-10-01 23:06:35 -0300

    Test optuna

--- tests/test_hyper_parameter_optimization.py
+++ tests/test_hyper_parameter_optimization.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 
+import numpy as np
 import pytest
 
 from src.models import hpo
@@ -11,23 +12,26 @@ def setup_mocked_mlflow():
         yield
 
 
-@pytest.fixture(name="mock_optuna")
-def setup_mocked_optuna():
-    with patch.object(hpo, "optuna"), patch.object(hpo, "TPESampler"):
-        yield
+@pytest.fixture(name="data")
+def generate_data():
+    rng = np.random.default_rng(seed=42)
+    features = rng.normal(size=(10, 4))
+    target = rng.binomial(n=1, p=0.5, size=10)
+
+    return features, target, features, target
 
 
-def test_optimize_logistic(mock_optuna):
-    # pylint: disable=unused-argument
-    hpo.optimize_logistic.fn(..., ..., ..., ..., 5)
+def test_optimize_logistic(data):
+    X_train, y_train, X_val, y_val = data
+    hpo.optimize_logistic.fn(X_train, y_train, X_val, y_val, 2)
 
 
-def test_optimize_xgboost(mock_optuna):
-    # pylint: disable=unused-argument
-    hpo.optimize_xgboost.fn(..., ..., ..., ..., 5)
+def test_optimize_xgboost(data):
+    X_train, y_train, X_val, y_val = data
+    hpo.optimize_xgboost.fn(X_train, y_train, X_val, y_val, 2)
 
 
-def test_hpo_main():
+def test_hpo_main_no_val():
     with (
         patch.object(hpo, "optimize_logistic") as mock_log,
         patch.object(hpo, "optimize_xgboost") as mock_xgb,
@@ -35,6 +39,21 @@ def test_hpo_main():
     ):
         mock_load.return_value = None, None
         hpo.main(["--train-data", "foo.pkl", "--num-trials", "50"])
+
+        mock_log.fn.assert_called_with(None, None, None, None, 50)
+        mock_xgb.fn.assert_called_with(None, None, None, None, 50)
+
+
+def test_hpo_main_with_val():
+    with (
+        patch.object(hpo, "optimize_logistic") as mock_log,
+        patch.object(hpo, "optimize_xgboost") as mock_xgb,
+        patch.object(hpo, "load_pickle") as mock_load,
+    ):
+        mock_load.return_value = None, None
+        hpo.main(
+            ["--train-data", "foo.pkl", "--val-data", "bar.pkl", "--num-trials", "50"]
+        )
 
         mock_log.fn.assert_called_with(None, None, None, None, 50)
         mock_xgb.fn.assert_called_with(None, None, None, None, 50)
